03cec69dede64393d69995c915b7e8f84db18e70
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-04-19 21:27:33 -0500

    change structure

--- make.sh
+++ make.sh
@@ -3,8 +3,8 @@
 set -e
 
 g++ compiler.cpp -o compiler
-./compiler example.bf example.s
-nasm -f elf64 example.s
-ld example.o -o example
-rm example.o
-./example
+./compiler example/example.bf example/example.s
+nasm -f elf64 example/example.s
+ld example/example.o -o example/example
+rm example/example.o
+./example/example
