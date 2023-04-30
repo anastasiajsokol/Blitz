97ac448e7b68e9a58da5a3114f99f296b6938a9b
anastasiajsokol <anastasia.j.sokol@gmail.com>
2023-04-29 22:25:39 -0500

    restructure

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-g++ compiler.cpp -o compiler
+g++ src/compiler.cpp -o compiler
 ./compiler example/example.bf example/example.s
 nasm -f elf64 example/example.s
 ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 -o example/example example/example.o -lc
